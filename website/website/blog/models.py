531cabf248daf40571d0e1838e25d96b5fcd23c9
Jose Silva <jose.silva4492fatec.sp.gov.br>
2024-08-23 13:52:30 -0300

    add models and tests

--- website/website/blog/models.py
+++ website/website/blog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-#STATUS = (
-#    (0,"Draft"),
-#    (1,"Publish")
-#)
-#
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -22,17 +22,3 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-#class Post(models.Model):
-#    title = models.CharField(max_length=200, unique=True)
-#    slug = models.SlugField(max_length=200, unique=True)
-#    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-#    updated_on = models.DateTimeField(auto_now=True)
-#    content = models.TextField()
-#    created_on = models.DateTimeField(auto_now_add=True)
-#    status = models.IntegerField(choices=STATUS, default=0)
-#
-#    class Meta:
-#        ordering = ['-created_on']
-#
-#    def __str__(self):
-#        return self.title
